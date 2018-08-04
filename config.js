3f8792fe61af7b362015094202ba25f922ebb9c8
xDeltaFox <Israelbz25@gmail.com>
2018-08-04 16:27:59 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-//- VLC Media Player
-exports.vlcPass = "rosebud"
-exports.vlcPort = 8080
-exports.vlcHost = "localhost"
-
-//- Xbox
-exports.xuid = "255c91af4c82a5c1dd10b33877f5e320b7bbab62"
-exports.xboxapi_key = "2533274956066993"
-exports.show_gamertag = true
